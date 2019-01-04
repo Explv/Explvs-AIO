cce613d8b0f6fa0718c686d98b4d7bcfea8ab642
Explv <Explv@osbot.org>
2019-01-04 21:44:42 +0000

    Version 1.9

--- AIO/src/org/aio/script/AIO.java
+++ AIO/src/org/aio/script/AIO.java
@@ -23,7 +23,7 @@ import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
 
-@ScriptManifest(author = "Explv", name = "Explv's AIO v1.8", info = "AIO", version = 1.8, logo = "http://i.imgur.com/58Zz0fb.png")
+@ScriptManifest(author = "Explv", name = "Explv's AIO v1.9", info = "AIO", version = 1.9, logo = "http://i.imgur.com/58Zz0fb.png")
 public class AIO extends Script {
 
     private Gui gui;
