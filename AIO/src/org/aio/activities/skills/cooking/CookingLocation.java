5da3373d2a0b3c1309fba7aaa0a7c601b964719a
Explv <Explv@osbot.org>
2019-01-13 17:40:18 +0000

    Add Lumbridge Range to cooking options Closes #40

--- AIO/src/org/aio/activities/skills/cooking/CookingLocation.java
+++ AIO/src/org/aio/activities/skills/cooking/CookingLocation.java
@@ -18,7 +18,9 @@ public enum CookingLocation {
     )), CookingObject.RANGE),
     EDGEVILLE_STOVE(new Location("Edgeville Stove", new Area(3077, 3493, 3079, 3496)), CookingObject.STOVE),
     EDGEVILLE_RANGE(new Location("Edgeville Range", new Area(new Position(3083, 3509, 1), new Position(3080, 3508, 1))), CookingObject.RANGE),
-    CATHERBY_RANGE(new Location("Catherby Range", new Area(2815, 3439, 2818, 3444)), CookingObject.RANGE);
+    CATHERBY_RANGE(new Location("Catherby Range", new Area(2815, 3439, 2818, 3444)), CookingObject.RANGE),
+    LUMBRIDGE_RANGE(new Location("Lumbridge Range", new Area(3205, 3212, 3212, 3217)), CookingObject.RANGE);
+
     public Location location;
     public CookingObject cookingObject;
 
