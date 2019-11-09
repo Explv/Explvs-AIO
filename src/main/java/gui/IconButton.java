eb256b0ae5a5884a0ffb91e9a98478052dec09b8
Explv <explv@osbot.org>
2019-11-09 16:10:24 +0000

    Circumvent OSBot's L&F to have buttons without backgrounds.

--- src/main/java/gui/IconButton.java
+++ src/main/java/gui/IconButton.java
@@ -9,7 +9,20 @@ import java.awt.image.BufferedImage;
 
 public class IconButton {
     public static JButton createButton(final String toolTip, final String icon, final String rolloverIcon, ActionListener callback) {
-        JButton button = new JButton();
+        JButton button = new JButton() {
+            @Override
+            public void paintComponent(Graphics g) {
+                if (this.model.isRollover()) {
+                    int drawX = (this.getWidth() / 2) - (this.getRolloverIcon().getIconWidth() / 2);
+                    int drawY = (this.getHeight() / 2) - (this.getRolloverIcon().getIconHeight() / 2);
+                    this.getRolloverIcon().paintIcon(this, g, drawX, drawY);
+                } else {
+                    int drawX = (this.getWidth() / 2) - (this.getIcon().getIconWidth() / 2);
+                    int drawY = (this.getHeight() / 2) - (this.getIcon().getIconHeight() / 2);
+                    this.getIcon().paintIcon(this, g, drawX, drawY);
+                }
+            }
+        };
         button.setToolTipText(toolTip);
 
         BufferedImage iconImage = ImageManager.loadImage(icon);
