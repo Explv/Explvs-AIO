09af946d9e4b054d280080f33f88f1374111e42a
Explv <explv@osbot.org>
2020-07-05 16:44:17 +0100

    Fix Cooks Assistant quest stuck after operating hopper controls.
    v3.2.3

--- src/main/java/script/AIO.java
+++ src/main/java/script/AIO.java
@@ -28,7 +28,7 @@ import java.util.List;
 @ScriptManifest(author = "Explv", name = "Explv's AIO " + AIO.VERSION, info = "AIO", version = 0, logo = "http://i.imgur.com/58Zz0fb.png")
 public class AIO extends Script {
 
-    public static final String VERSION = "v3.2.2";
+    public static final String VERSION = "v3.2.3";
 
     private Gui gui;
     private Paint paint;
